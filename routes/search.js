1e5540e1f0e2fde99eade924d94cb2701a43bb3d
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-08 10:30:29 +0100

    Add search by category

--- routes/search.js
+++ routes/search.js
@@ -28,7 +28,7 @@ router.get("/search", async (req, res, next) => {
 });
 
 /* GET search individual recipe  */
-router.get("/search/:id", async (req, res, next) => {
+router.get("/category", async (req, res, next) => {
     const id = req.params.id
     try {
         const axiosCall = await axios(
@@ -42,5 +42,32 @@ router.get("/search/:id", async (req, res, next) => {
         console.log(err);
     }
 });
+
+/* GET search category   */
+router.get("/search/:id", async (req, res, next) => {
+    const category = req.query.category
+    try {
+        const axiosCall = await axios(
+            `https://api.edamam.com/api/recipes/v2?type=public&app_id=${API_ID}&app_key=${API_KEY}&cuisineType=${category}&random=true`
+         );
+        const apiInfo = axiosCall.data.hits; 
+        // Get ID and push it in the object
+        const recipesInfo = apiInfo.map(element => {
+            const index = element.recipe.uri.lastIndexOf("_") + 1
+            const id = element.recipe.uri.substr(index)
+            element.recipe.id = id;
+            return element
+        })
+        res.status(200).json(recipesInfo[0]);
+      } catch (err) {
+        console.log(err);
+    }
+});
+
+//Time between 5-20
+//https://api.edamam.com/api/recipes/v2?type=public&app_id=aa72dc35&app_key=31d90debe31c5d2e3e9d132e26b9b768&time=5-20&random=true
+
+// Cocktail
+// https://api.edamam.com/api/recipes/v2?type=public&app_id=aa72dc35&app_key=31d90debe31c5d2e3e9d132e26b9b768&health=alcohol-cocktail&random=true
 
 module.exports = router;
